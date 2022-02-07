400c0a0716a4095c21d0a6d73c7ea1f7dd3fc3e3
andreaprozzo <andrea.prozzo@gmail.com>
2022-02-07 14:35:56 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super();
+    /*all'interno del costruttore, inserisco uno stato con una stringa , che vado a richiamare 
+    nel tag p con l'uso di this.state*/
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    /*con fetch() faccio una chiamata API all'indirizzo che contiene la lista dei mostri che restituisce la lista stessa*/
+    fetch('https://jsonplaceholder.typicode.com/users')
+    /*converto la response in formato json */
+    .then(response => response.json())
+    /*modifico lo stato utilizzando i dati presi con il fetch() e modificati in json */
+    .then(users => this.setState({monsters: users }))
+  }
+  /*Utilizzando l'arrow function nel metodo , evitiamo di fare il bind()  del metodo stesso. in questo modo stiamo rendendo il bind automatico*/
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
+  }
+
+  render() {
+    /*in questo modo applichiamo un filtro di selezione sulle cards  */
+    const { monsters, searchField } =  this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+  
+    return (
+      <div className="App">
+      <h1>Monster Rolodex</h1>
+        <SearchBox   
+         placeholder='search monsters'         
+         handleChange = { this.handleChange}/>
+      <CardList monsters={filteredMonsters} />
     </div>
-  );
+    )
+  } 
 }
 
 export default App;
